34d94178e28b6886926024a18cd3be1b0fc40df0
Assistantkiwi <chiekechidinma7@gmail.com>
2022-04-15 19:51:20 -0700

    Task 11 debugging

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -19,14 +19,14 @@ int main(void)
 	while (n % 2 == 0)
 	{
 		max = 2;
-			n /= 2;
+		n /= 2;
 	}
-	for (i = 3; i <= sqrt(n); i = 1 + 2)
+	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
 		while (n % i == 0)
 		{
 			max = i;
-			n = n / í;
+			n = n / i;
 		}
 	}
 	if (n > 2)
