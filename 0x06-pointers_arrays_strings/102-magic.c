405b0dfc5dab25fbc34c34f2f27d2137e94cba69
Assistantkiwi <chiekechidinma7@gmail.com>
2022-04-28 16:03:38 -0700

    Task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ * Return: 0 if success
+ */
+
 int main(void)
 {
 	int n;
@@ -18,6 +23,6 @@ int main(void)
 	 */
 	p[5] = 98;
 	/* ...so that this prints 98\n */
-	printf("a[2] = %d\n"; a[2]);
+	printf("a[2] = %d\n" | a[2]);
 	return (0);
 }
