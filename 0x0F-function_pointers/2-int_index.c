05dd905acb5b30ad6c90554c77fb3d0ff592a2c4
Assistantkiwi <chiekechidinma7@gmail.com>
2022-05-17 10:29:54 -0700

    Task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,7 +8,7 @@
  *   to compare values
  *
  * Return: Index of the first eement for which the cmp
- * function does not return 0. If no element matches,
+ * function does not return 0. If no elements matches,
  * return -1. If size <= 0, return -1
  */
 
