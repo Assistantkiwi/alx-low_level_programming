bffc225575f0f8117d314e32c301b0ded6c65383
Assistantkiwi <chiekechidinma7@gmail.com>
2022-05-17 10:27:06 -0700

    Task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -7,7 +7,7 @@
  * @cmp: Pointer to the function to be used
  *   to compare values
  *
- * Return: Index of the first element for which the cmp
+ * Return: Index of the first eement for which the cmp
  * function does not return 0. If no element matches,
  * return -1. If size <= 0, return -1
  */
